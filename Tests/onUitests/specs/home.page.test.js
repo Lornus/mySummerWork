f9c8dbc54a6473a9b6e120c3fc45a87ed0550887
Nikita Petrenko <57319234+Lornus@users.noreply.github.com>
2021-07-23 14:22:46 +0300

    Update home.page.test.js

--- Tests/onUitests/specs/home.page.test.js
+++ Tests/onUitests/specs/home.page.test.js
@@ -2,9 +2,11 @@ const homePage = require('../pageobjects/home.page')
 const assert = require('assert')
 
 describe('Home page tests', () => {
+before(async function (){
+    await homePage.goToHomePageUrl()
 
-    it('On home page must be "Home" button', async () => {
-        console.log("URL", await homePage.goToHomePageUrl())
+})
+    it('On home page must be "Home" button', async function ()  {
 
         expect(await homePage.checkHomePageDisplayed()).toBeTruthy()
         expect(await homePage.getHomePageAttribute("href")).toBe( "/")
@@ -12,36 +14,36 @@ describe('Home page tests', () => {
 
     })
 
-    it('On home page must be "Users" button', async () => {
+    it('On home page must be "Users" button', async function ()  {
         expect(await homePage.checkUserBtnClickable()).toBeTruthy()
         expect(await homePage.getUserBtnAttribute("href")).toBe("/users")
         expect (await homePage.getUserBtnText()).toBe( "Users")
     })
 
-    it('On home page must be "Planets" button', async () => {
+    it('On home page must be "Planets" button', async function ()  {
         expect(await homePage.checkPlanetsBtnClickable).toBeTruthy()
         expect(await homePage.getPlanetsBtnAttribute("href")).toBe("/planets")
         expect(await homePage.getPlanetsBtnText()).toBe("Planets")
     });
 
-    it('On home page must be "Races" button', async () => {
+    it('On home page must be "Races" button', async function ()  {
         expect(await homePage.checkRacesBtnClickable()).toBeTruthy()
         expect(await homePage.getRacesBtnAttribute("href")).toBe( "/races")
         expect(await homePage.getRacesBtnText()).toBe( "Races")
 
     });
-    it('On home page must be "About" button', async () => {
+    it('On home page must be "About" button', async function ()  {
         expect(await homePage.checkAboutBtnClickable()).toBeTruthy()
         expect(await homePage.getAboutBtnAttribute("href")).toBe( "/about")
         expect(await homePage.getAboutBtnText()).toBe("About")
 
     });
 
-    it('On home page must be footer with text "Planets"', async () => {
-        expect(await homePage.getFooterText()).toBe("🔞🔞Planets1🔞🔞")
+    it('On home page must be footer with text "Planets"', async function() {
+        expect(await homePage.getFooterText()).toBe("🔞🔞Planets🔞🔞")
     });
 
-        it('Displaying all pages above footer', async () => {
+        it('Displaying all pages above footer', async function () {
            const imagesDisplayed =  await homePage.getAllDownImages()
             return imagesDisplayed
     });
